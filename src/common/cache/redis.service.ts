bc1d521211cd116d2e77bde0a9ee944f820d28d0
Gabriel Matheus <gabrielmath14@outlook.com>
2026-03-03 21:34:15 -0300

    fix: redis.service.ts to accept redis_url the RAILWAY

--- src/common/cache/redis.service.ts
+++ src/common/cache/redis.service.ts
@@ -6,10 +6,12 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
   private client: Redis;
 
   onModuleInit() {
-    this.client = new Redis({
-      host: process.env.REDIS_HOST || 'localhost',
-      port: Number(process.env.REDIS_PORT) || 6379,
-    });
+    this.client = process.env.REDIS_URL
+      ? new Redis(process.env.REDIS_URL)
+      : new Redis({
+        host: process.env.REDIS_HOST || 'localhost',
+        port: Number(process.env.REDIS_PORT) || 6379,
+      });
   }
 
   onModuleDestroy() {
